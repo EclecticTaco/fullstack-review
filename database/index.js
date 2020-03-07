aaa68b15785dea24797fa30971921dc1955dcdf3
EclecticTaco <calvi5210@gmail.com>
2020-03-06 16:15:10 -0800

    redfined schema

--- database/index.js
+++ database/index.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+/* var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+}); */
+
+
+let repoSchema = new mongoose.Schema({
+  repo: {
+    userName: String, // user name
+    profilePic: String,  // URL to their profile pic
+    repoName: String, // string of their repo name
+    repoURL: String, // ULR string link to their repo
+    forks: Number, // number of times their repo has been forked
+    stars: Number // number of times their repo has been stared
+  }
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (result, callback) => {
+  console.log('result inside save passed form app.post: ', result)
+  callback(result)
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
