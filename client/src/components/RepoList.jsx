1c3cad302b508e264561dbb94996d23f43ed38e5
EclecticTaco <calvi5210@gmail.com>
2020-03-07 13:07:03 -0800

    css and render changes

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
 const RepoList = (props) => (
-  <div>
-    <h4>{props.repo.userName}</h4>
-    <img src={props.repo.profilePic} width="50" height="50"/>
-    <div>{props.repo.repoName}</div>
-    <div>{props.repo.repoURL}</div>
-    <div>{props.repo.forks}</div>
-    <div>{props.repo.stars}</div>
+  <div className="repoBox">
+    <img src={props.repo.profilePic} width="100" height="100"/>
+    <h2>{props.repo.userName}</h2>
+    <a href={props.repo.repoURL}>{props.repo.repoName}</a>
+    <div># of forks: {props.repo.forks}</div>
+    <div># of stars: {props.repo.stars}</div>
   </div>
 )
 
