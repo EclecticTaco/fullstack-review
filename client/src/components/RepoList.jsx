5d94e9c7389d7d3008dd5ef3cc5614944c35b357
EclecticTaco <calvi5210@gmail.com>
2020-03-07 13:34:47 -0800

    style changes

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const RepoList = (props) => (
   <div className="repoBox">
-    <img src={props.repo.profilePic} width="100" height="100"/>
-    <h2>{props.repo.userName}</h2>
-    <a href={props.repo.repoURL}>{props.repo.repoName}</a>
-    <div># of forks: {props.repo.forks}</div>
-    <div># of stars: {props.repo.stars}</div>
+    <div className="repo">
+    <img className="repo" src={props.repo.profilePic} width="100" height="100"/>
+    </div>
+    <h2 className="repo" >{props.repo.userName}</h2>
+    <div className="repo">
+    <a href={props.repo.repoURL} target="_blank">{props.repo.repoName}</a>
+    </div>
+    <div className="repo" ># of forks: {props.repo.forks}</div>
+    <div className="repo" ># of stars: {props.repo.stars}</div>
   </div>
 )
 
